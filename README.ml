88f3bde0b3ac568e53a805e64f84f5ef2eb54c66
Aurora Shidp-Wagenet, David Monarrez and matt groberg <matt@>
2019-01-08 16:27:37 -0800

    add read me

---
+++ README.ml
@@ -0,0 +1,31 @@
+# _Tacos De Los Meurtos_
+
+#### _Website for fictional taco stand, 01/08/2019_
+
+#### By _**Matt Groberg, David Monarrez, and Aurora Shido-Wagenet**_
+
+## Description
+
+_This website was made for the fictional Tacos De Los Meurtos taco stand in Mexico Indiana. It is being used to work in CSS styles and branching and merging practice._
+
+## Setup/Installation Requirements
+
+* _open dmonarrez.github.io/Tacos-De-Los-Muertos_
+
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and Contact Details
+
+_For help contact davidmonarrez3@gmail.com_
+
+## Technologies Used
+
+_Made using basic HTML and CSS styles, posted with GitHub pages._
+
+### License
+
+
+Copyright (c) MIT 2016 **_Matt Groberg, David Monarrez, and Aurora Shido-Wagenet_**
